e4c04809fd30d0f0d5a07348e136d71251361f70
dchen <dchen001213@gmail.com>
2019-01-15 21:15:57 -0500

    Fixed top background resizing, added phone number copying

--- res/DChen.js
+++ res/DChen.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    $(".various"+(new Date().getTime() % 2 + 1)).css("display", "block");
+    var increment = new Date().getTime() % 3 + 1;
+    $(".various"+ increment).css("display", "block");
 });
+
+function copyPhoneNumber() {
+    var phoneNum = document.getElementById("phoneNum");
+    phoneNum.select();
+    document.execCommand("copy");
+    alert("Copied phone number: " + phoneNum.value);
+}
